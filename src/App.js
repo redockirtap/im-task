86a09d6eb2f84c6c139a84ffde2cf45892c327cb
Partrickoder <redockirtap@gmail.com>
2022-11-17 19:19:07 +0200

    Add githubOauth() to redirect to GitHub authorization on a button click.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+const CLIENT_ID = "2e7940c77b22ef261b29";
 
 function App() {
+
+  useEffect(() => { // используется для проверки - получен ли код
+    const urlWithCode = window.location.search;
+    const urlParams = new URLSearchParams(urlWithCode);
+    const code = urlParams.get("code");
+    console.log(code);
+  }, []);
+
+  function githubOauth() {
+    window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={githubOauth}>Login via GitHub</button>
       </header>
     </div>
   );
